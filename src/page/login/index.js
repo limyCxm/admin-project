cdbe07028f528e09501d617898392c01ac02c29e
chengxueming <1475255466@qq.com>
2019-07-25 14:06:30 +0800

    update

---
+++ src/page/login/index.js
@@ -0,0 +1,23 @@
+ import React, { PureComponent } from 'react';
+ import './index.less'
+
+ class Login extends PureComponent {
+     constructor(props){
+         super(props);
+         this.state =  {
+             text:'123132'
+         }
+     }
+
+
+     render(){
+         return(
+             <div className="logiin">
+                123
+             </div>
+         )
+     }
+ }
+
+
+ export default Login;
